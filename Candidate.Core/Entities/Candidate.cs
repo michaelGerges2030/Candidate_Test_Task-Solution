1254b11290a79dd7a7bcd788fc307f3894e1cfd7
michaelGerges2030 <michaelbayo858@gmail.com>
2024-05-12 22:34:53 +0300

    Add project files.

---
+++ Candidate.Core/Entities/Candidate.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Candidate.Core.Entities
+{
+	public class Candidate
+	{
+		public int Id { get; set; }
+		public string FirstName { get; set; } = null!;
+		public string LastName { get; set; } = null!;
+		public string PhoneNumber { get; set; } = null!;
+		public string Email { get; set; } = null!;
+		public string TimeInterval { get; set; } = null!;
+		public string LinkedInProfileUrl { get; set; } = null!;
+		public string GitHubProfileUrl { get; set; } = null!;
+		public string Comment { get; set; } = null!;
+	}
+}
